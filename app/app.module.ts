09dc07b53dc604b88b959a8852851e80e2e6ea90
michaeldeongreen <michael.green@architectinginnovation.com>
2016-11-11 13:23:06 -0600

    *Completed routing portion of the tour of heroes tutoorial *Found a bug in the tutorial.  You need to put the non-relative path of the stylesheet for app.component.ts or the app will try to look for it in the root

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,37 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component';
-
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            },
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
@@ -39,10 +19,7 @@ import { DashboardComponent } from './dashboard.component';
         HeroDetailComponent,
         HeroesComponent
     ],
-    providers: [
-        HeroService
-    ],
+    providers: [HeroService],
     bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
     moduleId: module.id,
     selector: 'my-hero-detail',
     templateUrl: 'hero-detail.component.html',
+    styleUrls: ['hero-detail.component.css']
 })
 
 
