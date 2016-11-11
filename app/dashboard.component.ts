0492a9843564ae49679de8964ceb50c9ffa19355
michaeldeongreen <michael.green@architectinginnovation.com>
2016-11-10 19:56:14 -0600

    *Stopping point check in

---
+++ app/dashboard.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-dashboard',
+    templateUrl: 'dashboard.component.html',
+})
+
+export class DashboardComponent implements OnInit {
+    heroes: Hero[] = [];
+
+    constructor(private heroservice: HeroService) { }
+
+    ngOnInit(): void {
+        this.heroservice.getHeroes().then(h => this.heroes = h.slice(1, 5));
+    }
+}
